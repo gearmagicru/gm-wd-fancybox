57562a9276c99d8134f069ba1ae1d3fde4a4e5e6
Anton Tivonenko <anton.tivonenko@gmail.com>
2025-02-08 15:03:31 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Fancybox',
+    '{description}' => 'Fancybox lightbox',
+
+    // Settings
+    '{settings.title}' => 'Widget settings "Fancybox"',
+    // Settings: вкладки
+    'Common parameters' => 'Common parameters',
+    'Fancybox parameters' => 'Fancybox parameters',
+    'Version' => 'Version',
+    // Settings: вкладка / общие параметры
+    'Plug-in libraries' => 'Plug-in libraries',
+    'Connect libraries via' => 'Connect libraries via',
+    'Local' => 'Local',
+    'include jQuery library' => 'include jQuery library',
+    'include jQuery Easing animation library' => 'include jQuery Easing animation library',
+    'include mouse wheel library jQuery MouseWheel' => 'include mouse wheel library jQuery MouseWheel',
+    'Libraries must be included if they are not included in your site\'s theme' 
+        => 'Libraries must be included if they are not included in your site\'s theme',
+    // Settings: вкладка / параметры Fancybox
+    'Space between Fancybox wrapper and content, px.' => 'Space between Fancybox wrapper and content, px.',
+    'Space between viewport and Fancybox wrapper, px.' => 'Space between viewport and Fancybox wrapper, px.',
+    'When true, transparency of content is changed for elastic transitions' 
+        => 'When true, transparency of content is changed for elastic transitions',
+    'When true, galleries will be cyclic, allowing you to keep pressing next/back' 
+        => 'When true, galleries will be cyclic, allowing you to keep pressing next/back',
+    'Toggle close button' => 'Toggle close button',
+    'Toggle navigation arrows' => 'Toggle navigation arrows',
+    'Toggle if pressing Esc button closes Fancybox' => 'Применять кнопку Esc для закрытия Fancybox',
+    'If true, Fancybox is scaled to fit in viewport' 
+        => 'If true, Fancybox is scaled to fit in viewport',
+    'When true, Fancybox is centered while scrolling page' => 'When true, Fancybox is centered while scrolling page',
+    'Toggle if clicking the overlay should close Fancybox' => 'Toggle if clicking the overlay should close Fancybox',
+    'Toggle if clicking the content should close Fancybox' => 'Toggle if clicking the content should close Fancybox',
+    'Automatically resize an image when it is received' => 'Automatically resize an image when it is received',
+    'Speed of resizing when changing gallery items, ms.' => 'Speed of resizing when changing gallery items, ms.',
+    'Speed of the content fading while changing gallery items' => 'Speed of the content fading while changing gallery items',
+    'Fast' => 'Fast',
+    'Slow' => 'Slow',
+    'Easing used for elastic animations' => 'Easing used for elastic animations',
+    'Image appearance' => 'Image appearance',
+    'Hiding an image' => 'Hiding an image',
+    'Overlay' => 'Overlay',
+    'Toggle overlay' => 'Toggle overlay',
+    'Opacity of the overlay' => 'Opacity of the overlay',
+    'Color of the overlay' => 'Color of the overlay',
+    'Transition effect between images' => 'Transition effect between images',
+    'Transition effect when image appears' => 'Transition effect when image appears',
+    'Transition effect when hiding an image' => 'Transition effect when hiding an image',
+    'None' => 'None',
+    'Elastic' => 'Elastic',
+    'Fade' => 'Fade',
+    'Effect speed when image appears, ms.' => 'Effect speed when image appears, ms.',
+    'Effect speed when hiding an image, ms.' => 'Effect speed when hiding an image, ms.',
+    'Image title' => 'Image title',
+    'Toggle title' => 'Toggle title',
+    'The position of title' => 'The position of title',
+    'Outside' => 'Outside',
+    'Inside' => 'Inside',
+    'Before' => 'Before',
+    'Fancybox visible area dimensions' => 'Fancybox visible area dimensions',
+    'Width, px' => 'Width, px',
+    'Height, px' => 'Height, px',
+    'version' => 'version',
+    'site' => 'site',
+    'license' => 'license',
+    'License Agreement' => 'License Agreement',
+
+    // Settings: всплывающие сообщения / текст
+    'Widget settings successfully changed' => 'Widget settings "Fancybox" successfully changed.'
+];
---
+++ lang/text-ru_RU.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Этот файл является частью виджета веб-приложения GearMagic.
+ * 
+ * Пакет русской локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Fancybox',
+    '{description}' => 'Лайтбокс Fancybox',
+
+    // Settings
+    '{settings.title}' => 'Настройка виджета "Fancybox"',
+    // Settings: вкладки
+    'Common parameters' => 'Общие параметры',
+    'Fancybox parameters' => 'Параметры Fancybox',
+    'Version' => 'Версия',
+    // Settings: вкладка / общие параметры
+    'Plug-in libraries' => 'Подключаемые библиотеки',
+    'Connect libraries via' => 'Подключить библиотеки через',
+    'Local' => 'Локально',
+    'include jQuery library' => 'подключить библиотеку jQuery',
+    'include jQuery Easing animation library' => 'подключить библиотеку анимации jQuery Easing',
+    'include mouse wheel library jQuery MouseWheel' => 'подключить библиотеку работы колесика мыши jQuery MouseWheel',
+    'Libraries must be included if they are not included in your site\'s theme' 
+        => 'Библиотеки необходимо подключать в том случае, если они отсутствуют в теме вашего сайта',
+    // Settings: вкладка / параметры Fancybox
+    'Space between Fancybox wrapper and content, px.' => 'Пространство между обёрткой Fancybox и содержимым, пкс.',
+    'Space between viewport and Fancybox wrapper, px.' => 'Пространство между видимой областью и Fancybox, пкс.',
+    'When true, transparency of content is changed for elastic transitions' 
+        => 'Изменить прозрачность содержимого для переходов elastic',
+    'When true, galleries will be cyclic, allowing you to keep pressing next/back' 
+        => 'Цикличность галереи с появлением кнопок «Далее» / «Назад»',
+    'Toggle close button' => 'Отображать кнопку закрытия',
+    'Toggle navigation arrows' => 'Отображать кнопки навигации',
+    'Toggle if pressing Esc button closes Fancybox' => 'Применять кнопку Esc для закрытия Fancybox',
+    'If true, Fancybox is scaled to fit in viewport' 
+        => 'Масштабировать изображение по области просмотра',
+    'When true, Fancybox is centered while scrolling page' => 'Центрировать Fancybox при прокрутке страницы',
+    'Toggle if clicking the overlay should close Fancybox' => 'Закрывать Fancybox при нажатии кнопкой на перекрытии (overlay)',
+    'Toggle if clicking the content should close Fancybox' => 'Закрывать Fancybox при нажатии кнопкой на содержимом',
+    'Automatically resize an image when it is received' => 'Автоматически изменять размер изображения при его получении',
+    'Speed of resizing when changing gallery items, ms.' => 'Скорость изменения размера при смене элементов галереи',
+    'Speed of the content fading while changing gallery items' => 'Скорость исчезновения контента при смене элементов галереи, мс.',
+    'Fast' => 'Быстро',
+    'Slow' => 'Медленно',
+    'Easing used for elastic animations' => 'Плавность (easing) анимации при появлении / скрытии изображения',
+    'Image appearance' => 'Эффект при появлении изображения',
+    'Hiding an image' => 'Эффект при скрытии изображения',
+    'Overlay' => 'Перекрытие (задний фон)',
+    'Toggle overlay' => 'Показать перекрытие',
+    'Opacity of the overlay' => 'Прозрачность перекрытия',
+    'Color of the overlay' => 'Цвет перекрытия',
+    'Transition effect between images' => 'Эффект перехода между изображениями',
+    'Transition effect when image appears' => 'Эффект перехода при появлении изображения',
+    'Transition effect when hiding an image' => 'Эффект перехода при скрытии изображения',
+    'None' => 'Отсутствует',
+    'Elastic' => 'Нарастающий',
+    'Fade' => 'Затухание',
+    'Effect speed when image appears, ms.' => 'Скорость эффекта при появлении изображения, мс.',
+    'Effect speed when hiding an image, ms.' => 'Скорость эффекта при скрытии изображения, мс.',
+    'Image title' => 'Заголовок изображения',
+    'Toggle title' => 'Показывать заголовок',
+    'The position of title' => 'Положение заголовка',
+    'Outside' => 'Снаружи изображения',
+    'Inside' => 'Внутри изображения',
+    'Before' => 'Перед изображением',
+    'Fancybox visible area dimensions' => 'Размеры видимой области Fancybox',
+    'Width, px' => 'Ширина, пкс.',
+    'Height, px' => 'Высота, пкс.',
+    'version' => 'версия',
+    'site' => 'сайт',
+    'license' => 'лицензия',
+    'License Agreement' => 'Лицензионное соглашение',
+    
+    // Settings: всплывающие сообщения / текст
+    'Widget settings successfully changed' => 'Настройки виджета "Fancybox" успешно сохранены.'
+];
